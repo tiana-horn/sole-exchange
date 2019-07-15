fd5ff19cc75ebb5954c057c354cff0ded8672291
tiana-horn <tiana.horn@gmail.com>
2019-07-14 22:31:38 -0400

    AddHeroku

--- soleExchange/settings.py
+++ soleExchange/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -141,4 +142,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'core.User'
 
-
+django_heroku.settings(locals())
